adcda1a6c343a689872778f8c33508f5b35128dd
Douglas Bastos <dautodouglasdauto@gmail.com>
2023-10-07 13:12:30 -0300

    Primeira versão

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import './assets/css/base.css';
+import './assets/css/header.css';
+import './assets/css/main.css';
+import './assets/css/footer.css';
